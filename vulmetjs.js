25b40dc3351fb85417e6533ebb68fac030495d05
Amjad Alarori <2072708@talnet.nl>
2020-03-05 12:59:58 +0100

    klaar

---
+++ vulmetjs.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerHTML = "Mijn geweldige pagina";
+document.getElementById("subkop").innerHTML = "Mijn geweldige subkop";
+document.getElementById("img1").src = "../img1.jpg";
+document.getElementById("img2").src = "../img2.jpg";
